6e03558473b5dedefcc24f34f598b6fa1c923133
supakornnetsuwan <earth_killerdark@hotmail.com>
2023-04-14 20:05:33 +0700

    เปลี่ยนชื่อไฟล์ของ route authen

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,6 @@
 import express, { Response, Request } from "express";
 /* sub routes */
-import user from "./user"
+import auth from "./auth"
 import protect from "./protect"
 import home from './home'
 import summaries from './summaries'
@@ -13,7 +13,7 @@ router.get("/", (req: Request, res: Response) => {
     res.json({ message: "Welcome to the API" })
 })
 
-router.use("/user", user)
+router.use("/auth", auth)
 router.use("/protect", protect)
 router.use('/home', home) // home อันนี้คือเอาไว้ทำอะไรนะะ
 router.use('/post', post) // ควรเป็นคำพหุนาม /posts เพราะว่าเรามองว่า post มันเป็นเรื่องของโพสต์ในระบบ แต่เราก็ไม่ได้มองว่ามันเป็นเรื่องของโพสต์ๆ ไหนเจาะจงเหมือน /user ที่เป็นการเข้าถึง user เพียง 1 เดียว
