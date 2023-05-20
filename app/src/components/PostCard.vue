36346c216062144337958508d368114e6e99691a
supakornnetsuwan <earth_killerdark@hotmail.com>
2023-05-21 03:09:13 +0700

    fix : fix some unconsistent UI on PostCard

--- app/src/components/PostCard.vue
+++ app/src/components/PostCard.vue
@@ -10,19 +10,31 @@
     />
     <div class="m-2">
       <div class="flex flex-row w-full justify-between">
-        <div class="flex items-center space-x-2">
-          <h4 :class="['truncate lg:max-w-[15em]', postDetail?.follow_post.length > 0 ? 'text-blue-primary' : '']">{{ postDetail.post_title }}</h4>
-          <StarIcon v-if="postDetail?.follow_post.length > 0" class="h-3 aspect-square text-blue-primary bg-blue-soft p-1 rounded"/>
-        </div>
+        <h4
+          :class="[
+            'lg:max-w-[15em] truncate',
+            postDetail?.follow_post.length > 0 ? 'text-blue-primary' : '',
+          ]"
+        >
+          {{ postDetail.post_title }}
+        </h4>
+
         <div class="flex items-center">
           <h4 class="text-red-primary">อ่าน</h4>
           <ChevronRightIcon class="h-4 text-red-primary" />
         </div>
       </div>
       <p class="text-gray-3 published">
-        สร้างเมื่อวันที่ {{ new Date(postDetail.create_date).toLocaleDateString() }}
+        สร้างเมื่อวันที่
+        {{ new Date(postDetail.create_date).toLocaleDateString() }}
       </p>
-      <div class="mt-2"><SubjectTag :subject="postDetail.subject_name" /></div>
+      <div class="flex space-x-2 mt-2">
+        <SubjectTag :subject="postDetail.subject_name" />
+        <StarIcon
+          v-if="postDetail?.follow_post.length > 0"
+          class="h-3 aspect-square text-blue-primary bg-blue-soft p-1 rounded"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -45,14 +57,14 @@ export default {
   components: {
     SubjectTag,
     ChevronRightIcon,
-    StarIcon
+    StarIcon,
   },
   methods: {
     route() {
       this.$router.push(`/posts/${this.postDetail.post_id}`);
     },
     getImageFromUrl,
-    handleImageNotFound
+    handleImageNotFound,
   },
 };
 </script>
